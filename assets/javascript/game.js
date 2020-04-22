fa1778acc7b1c6059a3178af551b45fffbc527d6
Chelsea-exe <cjackson145@student.gsu.edu>
2020-04-22 00:21:28 -0400

    modified image files, js, & html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,67 @@
 //While typing the javascript I gotten errors of line 5 [setting up the range value of numbers] stating the ) isn't expected but per to my research the work we've done the symbol is need
 //I tried to debugging it, starting all over, redoing the javascript code, I didn't finish with the values of the rest of the crystals since I can't
 //figure out what "text" wasn't added correctly
-$(document).ready(function(){
-var random = Math.floor()*50+1)
 
-$('#random').text(Random);
+// var random = Math.floor()*50+1)
 
-var number1 = Math.floor(Math.random()*24+1);
-var number2 = Math.floor(Math.random()*24+1);
-var number3 = Math.floor(Math.random()*24+1);
-var number4 = Math.floor(Math.random()*24+1);
+// $('#random').text(Random);
 
-var total = 0;
-var wins = 0;
-var loses = 0;
-$('#Wins').html(Wins);
-$('#Losses').html(Losses);
+// var number1 = Math.floor(Math.random()*24+1);
+// var number2 = Math.floor(Math.random()*24+1);
+// var number3 = Math.floor(Math.random()*24+1);
+// var number4 = Math.floor(Math.random()*24+1);
 
-function reset(){
-    Random = Math.floor(Math.random()*50+1);
-    console.log(Random)
-    $('#random').html(Random);
-    number1 = Math.floor(Math.random()*24+1);
-    number2 = Math.floor(Math.random()*24+1);
-    number3 = Math.floor(Math.random()*24+1);
-    number4 = Math.floor(Math.randon()*24+1);
-    userTotal = 0;
-    $('#Totalscore').html(userTotal);
+// var total = 0;
+// var wins = 0;
+// var loses = 0;
+// $('#Wins').html(Wins);
+// $('#Losses').html(Losses);
 
-function yes(){
-alert("YOU'RE A WINNER!!");
-        wins++;
-        $('#Wins').html(wins);
-        reset();
-    }
-function welp(){
-alert("Welp, a Lose!");
-    losses++;
-    $('#Losses').html(losses);
-    reset()
-}
-$('#purpleC').on ('click', function(){
-    userTotal = userTotal + number1;
-    console.log("New Total = " + userTotal);
-    $('#Totalscore').html(userTotal);
-        if (userTotal === Random){
-          yes();
-        }
-        else if ( userTotal > Random){
-          welp();
-        }   
-  })  
-}
+// function reset(){
+//     Random = Math.floor(Math.random()*50+1);
+//     console.log(Random)
+//     $('#random').html(Random);
+//     number1 = Math.floor(Math.random()*24+1);
+//     number2 = Math.floor(Math.random()*24+1);
+//     number3 = Math.floor(Math.random()*24+1);
+//     number4 = Math.floor(Math.randon()*24+1);
+//     userTotal = 0;
+//     $('#Totalscore').html(userTotal);
+
+// function yes(){
+// alert("YOU'RE A WINNER!!");
+//         wins++;
+//         $('#Wins').html(wins);
+//         reset();
+//     }
+// function welp(){
+// alert("Welp, a Lose!");
+//     losses++;
+//     $('#Losses').html(losses);
+//     reset()
+// }
+// $('#purpleC').on ('click', function(){
+//     userTotal = userTotal + number1;
+//     console.log("New Total = " + userTotal);
+//     $('#Totalscore').html(userTotal);
+//         if (userTotal === Random){
+//           yes();
+//         }
+//         else if ( userTotal > Random){
+//           welp();
+//         }   
+//   })  
+// }
+
+
+$(document).ready(function() {
+
+  // alert("!!!");
+  var targetNumber = "";
+  $("#random").text(targetNumber);
+
+  var wins = 0;
+  var losses = 0;
+  var numberCounter = 0;
+  var options = [3, 11, 8, 19];
 })
